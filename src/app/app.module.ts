c10cd559ea23fa50bc2adb2cf8b3acceefebce1a
MariaSilvaPeres <maria.silva.peres@hotmail.com>
2021-03-25 15:31:33 +0000

    sdfasdas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home.component';
+import { BasicComponent } from './routes/basic.component';
+import { ContainedComponent } from './routes/contained.component';
+import { AdvancedComponent } from './routes/advanced.component';
 import { AppComponent } from './app.component';
 
+import { NotFoundComponent } from './routes/notfound.components';
+import { AnimboxComponent } from './animbox.component';
+
+const animationRoutes: Routes = [
+  {
+    path: 'animation-home',
+    component: HomeComponent,
+  },
+  {
+    path: 'animation-basics',
+    component: BasicComponent,
+  },
+  {
+    path: 'animation-contained',
+    component: ContainedComponent,
+  },
+  {
+    path: 'animation-advanced',
+    component: AdvancedComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/animation-home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AnimboxComponent,
+    HomeComponent,
+    BasicComponent,
+    ContainedComponent,
+    AdvancedComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(animationRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
